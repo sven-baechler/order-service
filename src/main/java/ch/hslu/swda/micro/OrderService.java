32becbf253b5e6a3bf90165fd052c30c2be5c256
Henseler Patrick <patrick.henseler@stud.hslu.ch>
2024-04-04 18:53:21 +0200

    Refactoring: Created const for same strings

--- src/main/java/ch/hslu/swda/micro/OrderService.java
+++ src/main/java/ch/hslu/swda/micro/OrderService.java
@@ -2,7 +2,6 @@ package ch.hslu.swda.micro;
 
 import ch.hslu.swda.bus.BusConnector;
 import ch.hslu.swda.bus.RabbitMqConfig;
-import ch.hslu.swda.services.OrderCreateService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,11 +11,12 @@ import java.util.concurrent.TimeoutException;
 public final class OrderService  implements AutoCloseable {
 
     private static final Logger LOG = LoggerFactory.getLogger(OrderService.class);
+    private static final String START_LISTENING_LOG_MESSAGE = "Starting listening for messages with routing [{}]";
+    private static final String QUEUE_NAME_PREFIX = "Starting listening for messages with routing [{}]";
     private final String exchangeName;
     private final BusConnector bus;
 
     public OrderService() throws IOException, TimeoutException {
-
         // thread info
         String threadName = Thread.currentThread().getName();
         LOG.debug("[Thread: {}] Service started", threadName);
@@ -39,22 +39,22 @@ public final class OrderService  implements AutoCloseable {
     }
 
     private void receiveOrderReceived() throws IOException {
-        LOG.debug("Starting listening for messages with routing [{}]", Routes.ORDER_RECEIVED);
-        this.bus.listenFor(exchangeName, "OrderService <- " + Routes.ORDER_RECEIVED, Routes.ORDER_RECEIVED, new OrderReceiver(exchangeName, bus));
+        LOG.debug(START_LISTENING_LOG_MESSAGE, Routes.ORDER_RECEIVED);
+        this.bus.listenFor(exchangeName, QUEUE_NAME_PREFIX + Routes.ORDER_RECEIVED, Routes.ORDER_RECEIVED, new OrderReceiver(exchangeName, bus));
     }
 
     private void receiveArticleStockUpdateRequired() throws IOException {
-        LOG.debug("Starting listening for messages with routing [{}]", Routes.ARTICLE_STOCK_UPDATE_REQUIRED);
-        this.bus.listenFor(exchangeName, "OrderService <- " + Routes.ARTICLE_STOCK_UPDATE_REQUIRED, Routes.ARTICLE_STOCK_UPDATE_REQUIRED, new ArticleStockRequiredReceiver(exchangeName, bus));
+        LOG.debug(START_LISTENING_LOG_MESSAGE, Routes.ARTICLE_STOCK_UPDATE_REQUIRED);
+        this.bus.listenFor(exchangeName, QUEUE_NAME_PREFIX + Routes.ARTICLE_STOCK_UPDATE_REQUIRED, Routes.ARTICLE_STOCK_UPDATE_REQUIRED, new ArticleStockRequiredReceiver(exchangeName, bus));
     }
 
     private void receiveOrderEntryUpdated() throws IOException {
-        LOG.debug("Starting listening for messages with routing [{}]", Routes.ORDER_ENTRY_UPDATED);
-        this.bus.listenFor(exchangeName, "OrderService <- " + Routes.ORDER_ENTRY_UPDATED, Routes.ORDER_ENTRY_UPDATED, new OrderEntryUpdatedReceiver(exchangeName, bus));
+        LOG.debug(START_LISTENING_LOG_MESSAGE, Routes.ORDER_ENTRY_UPDATED);
+        this.bus.listenFor(exchangeName, QUEUE_NAME_PREFIX + Routes.ORDER_ENTRY_UPDATED, Routes.ORDER_ENTRY_UPDATED, new OrderEntryUpdatedReceiver(exchangeName, bus));
     }
 
     private void orderListRequestedReceived() throws IOException {
-        LOG.debug("Starting listening for messages with routing [{}]", Routes.ORDER_LIST_REQUESTED);
-        this.bus.listenFor(exchangeName, "OrderService <- " + Routes.ORDER_LIST_REQUESTED, Routes.ORDER_LIST_REQUESTED, new OrderListRequestedReceiver(exchangeName, bus));
+        LOG.debug(START_LISTENING_LOG_MESSAGE, Routes.ORDER_LIST_REQUESTED);
+        this.bus.listenFor(exchangeName, QUEUE_NAME_PREFIX + Routes.ORDER_LIST_REQUESTED, Routes.ORDER_LIST_REQUESTED, new OrderListRequestedReceiver(exchangeName, bus));
     }
 }
