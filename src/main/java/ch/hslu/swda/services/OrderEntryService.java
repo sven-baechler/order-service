c6c1a0a29bf78c4478a6460cd7e732923536d72d
Sven Bächler <sven.baechler@stud.hslu.ch>
2024-04-04 18:12:34 +0200

    Refactoring: mongo logik von service logik getrennt, tests geschrieben

--- src/main/java/ch/hslu/swda/services/OrderEntryService.java
+++ src/main/java/ch/hslu/swda/services/OrderEntryService.java
@@ -1,11 +1,15 @@
 package ch.hslu.swda.services;
 
+import ch.hslu.swda.entities.Order;
+import ch.hslu.swda.entities.OrderEntry;
 import ch.hslu.swda.entities.OrderStatus;
 import ch.hslu.swda.logging.LogService;
 import ch.hslu.swda.messages.OrderEntryUpdatedMessage;
 import ch.hslu.swda.mongo.MongoService;
 import org.bson.types.ObjectId;
 import org.slf4j.Logger;
+
+import java.util.Optional;
 
 public class OrderEntryService {
     private static Logger LOG;
@@ -23,9 +27,38 @@ public class OrderEntryService {
         ObjectId articleId = new ObjectId(orderEntryUpdatedMessage.getArticleId());
         OrderStatus status = orderEntryUpdatedMessage.getStatus();
 
-        boolean orderEntryUpdated = this.mongoService.updateOrderEntryStatus(orderId, articleId, status);
-        if (orderEntryUpdated) {
-            this.mongoService.updateOrderStatus(orderId);
+        Order order = this.mongoService.getOrder(orderId);
+
+        if (order != null) {
+            if (this.updateOrderEntryStatus(order, articleId, status)) {
+                this.updateOrderStatus(order);
+            }
+        }
+    }
+
+    private boolean updateOrderEntryStatus(Order order, ObjectId articleId, OrderStatus status) {
+        Optional<OrderEntry> matchingEntry = order.getEntries().stream()
+                .filter(entry -> entry.getArticleId().equals(articleId))
+                .findFirst();
+
+        if (matchingEntry.isPresent()) {
+            matchingEntry.get().setStatus(status);
+            return this.mongoService.updateOrder(order);
+        } else {
+            this.logService.error(String.format("could not find article with id %s in order with id %s", articleId.toString(), order.getId().toString()));
+            return false;
+        }
+    }
+    private void updateOrderStatus(Order order) {
+        if (order.getEntries().stream().allMatch(oe -> oe.getStatus() == OrderStatus.READY_TO_DELIVER)) {
+            order.setStatus(OrderStatus.READY_TO_DELIVER);
+
+            if (this.mongoService.updateOrder(order)) {
+                this.logService.info("All order entries from order %s are ready to deliver, order status changed to ready to deliver", order.getId().toString()).send();
+            }
+        }
+        else {
+            this.logService.info("Not all order entries from order %s are ready to deliver yet, order status stays the same", order.getId().toString()).send();
         }
     }
 }
--- src/test/java/ch/hslu/swda/services/OrderEntryServiceTest.java
+++ src/test/java/ch/hslu/swda/services/OrderEntryServiceTest.java
@@ -1,6 +1,10 @@
 package ch.hslu.swda.services;
 
+import ch.hslu.swda.entities.Order;
+import ch.hslu.swda.entities.OrderEntry;
 import ch.hslu.swda.entities.OrderStatus;
+import ch.hslu.swda.logging.Log;
+import ch.hslu.swda.logging.LogService;
 import ch.hslu.swda.messages.OrderEntryUpdatedMessage;
 import ch.hslu.swda.mongo.MongoService;
 import org.bson.types.ObjectId;
@@ -10,12 +14,20 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Arrays;
+
 import static org.mockito.Mockito.*;
 
 class OrderEntryServiceTest {
 
     @Mock
     private MongoService mongoService;
+
+    @Mock
+    private LogService logService;
+
+    @Mock
+    private Log log;
 
     @InjectMocks
     private OrderEntryService orderEntryService;
@@ -26,44 +38,114 @@ class OrderEntryServiceTest {
     }
 
     @Test
-    void updateOrderEntryThenUpdateOrderStatus() {
+    void updateOrderEntryStatusThenUpdateOrderStatus() {
         // Arrange
-        String orderId = new ObjectId().toString();
-        String articleId = new ObjectId().toString();
-        OrderStatus status = OrderStatus.DELIVERED;
-        doReturn(true).when(mongoService).updateOrderEntryStatus(new ObjectId(orderId), new ObjectId(articleId), status);
+        this.mockLogService();
+
+        ObjectId orderId = new ObjectId("000000000000000000000000");
+        ObjectId articleId = new ObjectId("000000000000000000000000");
+        OrderStatus status = OrderStatus.READY_TO_DELIVER;
+
+        Order order = new Order();
+        order.setId(orderId);
+        order.setStatus(OrderStatus.ORDERED);
+        OrderEntry orderEntry1 = new OrderEntry();
+        orderEntry1.setArticleId(articleId);
+        orderEntry1.setStatus(OrderStatus.ORDERED);
+        order.setEntries(Arrays.asList(orderEntry1));
+
+        doReturn(order).when(mongoService).getOrder(orderId);
+        doReturn(true).when(mongoService).updateOrder(order);
 
         OrderEntryUpdatedMessage message = new OrderEntryUpdatedMessage();
-        message.setOrderId(orderId);
-        message.setArticleId(articleId);
+        message.setOrderId(orderId.toString());
+        message.setArticleId(articleId.toString());
         message.setStatus(status);
 
         // Act
         orderEntryService.handleOrderEntryUpdated(message);
 
         // Assert
-        verify(mongoService, times(1)).updateOrderEntryStatus(new ObjectId(orderId), new ObjectId(articleId), status);
-        verify(mongoService, times(1)).updateOrderStatus(new ObjectId(orderId));
+        verify(mongoService, times(2)).updateOrder(order);
+
+        // TODO: cannot verify invocation of private methods in OrderEntryService ???
     }
 
     @Test
-    void updateOrderEntryDontUpdateOrderStatus() {
+    void updateOrderEntryStatusDontUpdateOrderStatus() {
         // Arrange
-        String orderId = new ObjectId().toString();
-        String articleId = new ObjectId().toString();
-        OrderStatus status = OrderStatus.DELIVERED;
-        doReturn(false).when(mongoService).updateOrderEntryStatus(new ObjectId(orderId), new ObjectId(articleId), status);
+        this.mockLogService();
+
+        ObjectId orderId = new ObjectId("000000000000000000000000");
+        ObjectId articleId = new ObjectId("000000000000000000000000");
+        OrderStatus status = OrderStatus.READY_TO_DELIVER;
+
+        Order order = new Order();
+        order.setId(orderId);
+        order.setStatus(OrderStatus.ORDERED);
+        OrderEntry orderEntry1 = new OrderEntry();
+        orderEntry1.setArticleId(articleId);
+        orderEntry1.setStatus(OrderStatus.ORDERED);
+        OrderEntry orderEntry2 = new OrderEntry();
+        orderEntry2.setArticleId(new ObjectId("111111111111111111111111"));
+        orderEntry2.setStatus(OrderStatus.ORDERED);
+        order.setEntries(Arrays.asList(orderEntry1, orderEntry2));
+
+        doReturn(order).when(mongoService).getOrder(orderId);
+        doReturn(true).when(mongoService).updateOrder(order);
 
         OrderEntryUpdatedMessage message = new OrderEntryUpdatedMessage();
-        message.setOrderId(orderId);
-        message.setArticleId(articleId);
+        message.setOrderId(orderId.toString());
+        message.setArticleId(articleId.toString());
         message.setStatus(status);
 
         // Act
         orderEntryService.handleOrderEntryUpdated(message);
 
         // Assert
-        verify(mongoService, times(1)).updateOrderEntryStatus(new ObjectId(orderId), new ObjectId(articleId), status);
-        verify(mongoService, times(0)).updateOrderStatus(new ObjectId(orderId));
+        verify(mongoService, times(1)).updateOrder(order);
+
+        // TODO: cannot verify invocation of private methods in OrderEntryService ???
+    }
+
+    @Test
+    void updateOrderEntryStatusNoSuchArticle() {
+        // Arrange
+        this.mockLogService();
+
+        ObjectId mockOrderId = new ObjectId("000000000000000000000000");
+        ObjectId mockArticleId = new ObjectId("000000000000000000000000");
+        OrderStatus mockOrderStatus = OrderStatus.ORDERED;
+        OrderStatus mockArticleStatus = OrderStatus.ORDERED;
+        Order mockOrder = new Order();
+        mockOrder.setId(mockOrderId);
+        mockOrder.setStatus(mockOrderStatus);
+        OrderEntry orderEntry1 = new OrderEntry();
+        orderEntry1.setArticleId(mockArticleId);
+        orderEntry1.setStatus(mockArticleStatus);
+        mockOrder.setEntries(Arrays.asList(orderEntry1));
+
+        doReturn(mockOrder).when(mongoService).getOrder(mockOrderId);
+        doReturn(true).when(mongoService).updateOrder(mockOrder);
+
+        ObjectId orderId = new ObjectId("000000000000000000000000");
+        ObjectId articleId = new ObjectId("111111111111111111111111");
+        OrderStatus articleStatus = OrderStatus.ORDERED;
+        OrderEntryUpdatedMessage message = new OrderEntryUpdatedMessage();
+        message.setOrderId(orderId.toString());
+        message.setArticleId(articleId.toString());
+        message.setStatus(articleStatus);
+
+        // Act
+        orderEntryService.handleOrderEntryUpdated(message);
+
+        // Assert
+        verify(mongoService, times(0)).updateOrder(mockOrder);
+        verify(logService, times(1)).error(String.format("could not find article with id %s in order with id %s", articleId, orderId));
+    }
+
+    private void mockLogService() {
+        doReturn(this.log).when(logService).info(anyString(), any());
+        doNothing().when(this.log).send();
     }
 }
